7851eaf30790452034d5ff31dce8f018ee13b6a8
Peter Gribanov <wcode404@gmail.com>
2017-04-03 10:27:55 +0000

    Apply fixes from StyleCI

--- tests/DependencyInjection/AnimeDbPaginationExtensionTest.php
+++ tests/DependencyInjection/AnimeDbPaginationExtensionTest.php
@@ -6,6 +6,7 @@
  * @copyright Copyright (c) 2011, Peter Gribanov
  * @license   http://opensource.org/licenses/MIT
  */
+
 namespace AnimeDb\Bundle\PaginationBundle\Tests\DependencyInjection;
 
 use AnimeDb\Bundle\PaginationBundle\DependencyInjection\AnimeDbPaginationExtension;
